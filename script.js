9881fb1f3217c67eb288a11c4bebf71e167cb2a9
Mofikoya Oluwatosin <58105807+oluwatosin-ctrl@users.noreply.github.com>
2020-05-30 17:55:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function rpsGame(YourChoice){
+    console.log(YourChoice);
+    var HumanChoice, BotChoice
+    HumanChoice= YourChoice.id
+    BotChoice = numbertochoice(randomtoRpsinteger());
+    console.log('Computer choice:', BotChoice)
+    result = decidewinner(HumanChoice,BotChoice) 
+    console.log(results)
+    //message = finalmessage(result) //{message:'You Won!', color: Green}
+    //rpsFrontend(YourChoice.id,BotChoice,message);
+
+   
+
+}
+
+function randomtoRpsinteger(){
+    return Math.floor(Math.random()*3);
+}
+function numbertochoice(number){
+    return ['rock','paper','scissors'][number]
+}
+function decidewinner(YourChoice,BotChoice){
+    var Rpsdatatbase ={
+    'rock': {'scissors': 1, 'rock':0.5, 'paper':0 },
+    'paper': {'rock': 1, 'paper':0.5, 'scissors': 1 },
+    'scissors': {'paper':1,'scissors':0.5,'rock': 0}
+    }
+    var YourScore = Rpsdatatbase[YourChoice][BotChoice]
+    var BotScore = Rpsdatatbase[BotChoice][YourChoice]
+
+    return [YourScore,BotScore]
+
+}
